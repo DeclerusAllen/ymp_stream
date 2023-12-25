b0766b02261c03cc4534b199613d36e22ec8c61a
Allen DECLERUS <120142571+DeclerusAllen@users.noreply.github.com>
2023-12-25 10:20:35 -0500

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    $(".album-poster").on('click', function(e){
+        var dataSwitchId = $(this).attr('data-switch');
+        ap.list.switch(dataSwitchId);
+        ap.play();
+        $("#aplayer").addClass('showPlayer');
+    });
+
+    const ap = new APlayer({
+        container: document.getElementById('aplayer'),
+        listFolded: true,
+        audio: [
+            {
+                name: 'Rewrite the stars',
+                artist: 'Artist Name',
+                url: 'source/Rock.mp3',
+                cover: 'pictures/A.jpg'
+            },
+            {
+                name: 'Ideal',
+                artist: 'Artist Name',
+                url: 'source/Ideal.mp3',
+                cover: 'pictures/logo.png'
+            },
+          
+            {
+                name: 'OPPMV',
+                artist: 'Stephadem',
+                url: 'source/OPPM.mp3',
+                cover: 'https://images.pexels.com/photos/838696/pexels-photo-838696.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            },
+            {
+				name: 'Rap di',
+				artist: 'Artist Name',
+				url: 'source/Rap.mp3',
+				cover: 'https://images.pexels.com/photos/1370545/pexels-photo-1370545.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+			},
+			{
+				name: 'DRil ',
+				artist: 'Artist Name',
+				url: 'source/ffee.mp3',
+				cover: 'https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+			},
+			{
+				name: 'Our God ',
+				artist: 'YMP',
+				url: 'source/Our God.mp3',
+				cover: 'pictures/our',
+			},
+        ]
+    });
+});
