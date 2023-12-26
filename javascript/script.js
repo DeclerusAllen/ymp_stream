adbac74dca0811167f4f85dee4162cdba9ff1561
Allen DECLERUS <120142571+DeclerusAllen@users.noreply.github.com>
2023-12-25 21:33:59 -0500

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
 				cover: 'https://images.pexels.com/photos/1370545/pexels-photo-1370545.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
 			},
 			{
-				name: 'DRil ',
+				name: 'DRILL ',
 				artist: 'Artist Name',
 				url: 'source/ffee.mp3',
 				cover: 'https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
@@ -45,8 +45,56 @@ $(document).ready(function() {
 				name: 'Our God ',
 				artist: 'YMP',
 				url: 'source/Our God.mp3',
-				cover: 'pictures/our',
+				cover: 'pictures/our.png',
+			},
+            {
+				name: 'Unity',
+				artist: 'Alan Walker (cover by YMP)',
+				url: 'source/Unity.mp3',
+				cover: 'pictures/unity.png',
+			},
+            {
+				name: 'Wey',
+				artist: 'YMP',
+				url: 'source/wey.mp3',
+				cover: 'pictures/wey.png',
+			},
+            {
+				name: 'Oceans',
+				artist: 'YMP',
+				url: 'source/oceans.mp3',
+				cover: 'pictures/oceans.jpeg',
 			},
         ]
     });
 });
+
+
+$(document).ready(function() {
+    $(".album-posters").on('click', function(e){
+        var dataSwitchId = $(this).attr('data-switches');
+        ap.list.switch(dataSwitchId);
+        ap.play();
+        $("#aplayer").addClass('showPlayer');
+    });
+
+    const ap = new APlayer({
+        container: document.getElementById('aplayer'),
+        listFolded: true,
+        audio: [
+            {
+				name: 'Oceans',
+				artist: 'YMP',
+				url: 'source/oceans.mp3',
+				cover: 'pictures/oceans.jpeg',
+			},
+            {
+				name: 'Gospel',
+				artist: 'YMP',
+				url: 'source/piano.mp3',
+				cover: 'https://images.pexels.com/photos/838702/pexels-photo-838702.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+			},
+        ]
+    });
+});
+
